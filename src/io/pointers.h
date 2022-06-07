6665f4e5c7292c3af990cba02abf071899825a34
sunaD630 <undcircle.child1103@gmail.com>
2022-06-07 16:06:58 +0900

    add pthread

--- src/io/pointers.h
+++ src/io/pointers.h
@@ -8,14 +8,14 @@ typedef struct sound_io_pointers {
 } SoundIO;
 
 typedef struct client_io_pointers {
-    SoundIO *sound;//popenの返したFILEポインタ
-    int sfd;//サーバーのfd
+    SoundIO *sound;
+    int sfd;
 } ClientIO;
 
 typedef struct server_io_pointers {
-    SoundIO *sound;//popenの返したFILEポインタ
-    int clinet_socket_fd;//acceptの返り値
-    int server_socket_fd;//サーバーのsocket
+    SoundIO *sound;
+    int clinet_socket_fd;
+    int server_socket_fd;
 } ServerIO;
 
 SoundIO *empty_sound_io();
